20744472f6e74e9bd5a369a98589d0d1efb65c72
samuelchou <samuelchou@hotmail.com.tw>
2020-10-27 00:49:25 +0800

    <Add> navigation_main 新增頁面，並設定Action. NFeedbackFragment 現在可以前往 ResultFragment 並監聽返回鍵了。

--- app/src/main/java/studio/ultoolapp/fragbook/NFeedbackFragment.kt
+++ app/src/main/java/studio/ultoolapp/fragbook/NFeedbackFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment.findNavController
 
 class NFeedbackFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +15,13 @@ class NFeedbackFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_n_feedback, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = findNavController(this)
+        view.findViewById<Button>(R.id.btnYes).setOnClickListener {
+            navController.navigate(R.id.action_NFeedbackFragment_to_resultFragment)
+        }
     }
 }
